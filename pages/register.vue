5781eff46c59574601cc36e8b6fdd977aaaf2bf9
chenxin <lightnoway@qq.com>
2020-09-17 18:08:03 +0800

    注册过程 ,与登录合并减少重复代码 视图: 表单,错误信息 数据 user,error 业务: 发请求, 跳转, 显示错误信息

--- pages/register.vue
+++ pages/register.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="auth-page">
     <div class="container page">
-      <div v-if="isLogin" class="row">
+      <div class="row">
         <div class="col-md-6 offset-md-3 col-xs-12">
-          <h1 class="text-xs-center">Sign in</h1>
+          <h1 class="text-xs-center">{{isLogin?"Sign in": "Sign up"}}</h1>
           <p class="text-xs-center">
-            <nuxt-link to="/register">Need an account?</nuxt-link>
+            <nuxt-link v-if="isLogin" to="/register">Need an account?</nuxt-link>
+            <nuxt-link v-else to="/login">Have an account?</nuxt-link>
           </p>
 
           <ul class="error-messages">
@@ -13,7 +14,16 @@
               <li v-for="(message,index) in messages" :key="index">{{field +" "+ message}}</li>
             </template>
           </ul>
-          <form @submit.prevent="onLogin">
+          <form @submit.prevent="onSubmit">
+            <fieldset class="form-group" v-if="!isLogin">
+              <input
+                class="form-control form-control-lg"
+                type="text"
+                placeholder="Your Name"
+                v-model="user.username"
+                required
+              />
+            </fieldset>
             <fieldset class="form-group">
               <input
                 class="form-control form-control-lg"
@@ -32,32 +42,7 @@
                 required
               />
             </fieldset>
-            <button class="btn btn-lg btn-primary pull-xs-right">Sign in</button>
-          </form>
-        </div>
-      </div>
-      <div v-else class="row">
-        <div class="col-md-6 offset-md-3 col-xs-12">
-          <h1 class="text-xs-center">Sign up</h1>
-          <p class="text-xs-center">
-            <nuxt-link to="/login">Have an account?</nuxt-link>
-          </p>
-
-          <ul class="error-messages">
-            <li>That email is already taken</li>
-          </ul>
-
-          <form>
-            <fieldset class="form-group">
-              <input class="form-control form-control-lg" type="text" placeholder="Your Name" />
-            </fieldset>
-            <fieldset class="form-group">
-              <input class="form-control form-control-lg" type="text" placeholder="Email" />
-            </fieldset>
-            <fieldset class="form-group">
-              <input class="form-control form-control-lg" type="password" placeholder="Password" />
-            </fieldset>
-            <button class="btn btn-lg btn-primary pull-xs-right">Sign up</button>
+            <button class="btn btn-lg btn-primary pull-xs-right">{{isLogin?"Sign in":"Sign up"}}</button>
           </form>
         </div>
       </div>
@@ -65,7 +50,7 @@
   </div>
 </template>
 <script>
-import { login } from "@/api/user";
+import { login, register } from "@/api/user";
 export default {
   data() {
     return {
@@ -85,9 +70,10 @@ export default {
     },
   },
   methods: {
-    async onLogin() {
+    async onSubmit() {
+      throw new Error('som wrong');
       try {
-        const data = await login({
+        const data = await (this.isLogin ? login : register)({
           user: this.user,
         });
         //保存token
